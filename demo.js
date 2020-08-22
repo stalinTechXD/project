bd47118e1b3ca549a93745424576901e2104fcbd
stali1234 <stalin.sangeethraj@gmail.com>
2020-08-22 13:14:25 -0400

    initial commit

---
+++ demo.js
@@ -0,0 +1,6 @@
+var name = prompt("enter your name");
+var age = prompt("enter your age");
+agedays = age*365.25;
+remainingdays = (60-age)*365.25;
+
+alert("hello"+name+"\n your current age is "+agedays+ "days\n you will be there on the earth only"+remainingdays+"no one can change including god also" );
